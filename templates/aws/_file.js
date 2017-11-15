1c229fa4dbf94b875a20333d5333652c0fc36f60
Brian C Sparks <brian.c.sparks@hp.com>
2017-11-15 13:42:24 -0800

    Added aws

--- templates/aws/_file.js
+++ templates/aws/_file.js
@@ -4,11 +4,14 @@
  */
 const sg                      = require('sgsg');
 const _                       = sg._;
+const AWS                     = require('aws-sdk');
 
 const argvGet                 = sg.argvGet;
 const argvExtract             = sg.argvExtract;
 const setOnn                  = sg.setOnn;
 const deref                   = sg.deref;
+
+var   s3                      = new AWS.S3();
 
 var lib = {};
 
@@ -16,14 +19,19 @@ var lib = {};
 lib.myFavoriteFunction = function(argv, context, callback) {
 
   return sg.__run2({}, callback, [function(result, next, last, abort) {
-    return next();
+
+    const params = {};
+
+    return s3.getObject(params, function(err, data) {
+      if (err)        { return abort(err, 'functionname.calledfunctionname'); }
+      return next();
+    });
 
   }, function(result, next, last, abort) {
     return next();
-
   }], function abort(err, msg) {
+    if (err && msg)        { return sg.die(err, callback, msg); }
     return callback();
-
   });
 };
 
