0d8da19fdb0d14c7f2157f4eccf18a82e3ecd1ad
Brian C Sparks <brian.c.sparks@hp.com>
2018-08-29 13:50:32 -0700

    wip

--- templates-active/sam-init/index.js
+++ templates-active/sam-init/index.js
@@ -1,18 +1,42 @@
 
 const { qm }                  = require('quick-merge');
 
-const srcMod                  = require('./src');
+var tDir;
+var samYamlFile;
+var lambdaJsFile;
+
+var phases = {};
 
 exports.boot = async function(jetpackA, argv, utils) {
+  const { JetpackFile } = utils;
+  tDir    = jetpackA.cwd(__dirname);
+
+  samYamlFile   = new JetpackFile(tDir, 'sam.yaml');
+  lambdaJsFile  = new JetpackFile(tDir, 'src/lambda.js');
+
   return {
     dest: jetpackA.cwd()
   };
 }
 
-exports.main = async function(jetpack, argv, phase, current, utils) {
-  const srcResult = await srcMod.main(jetpack, argv, phase, current, utils);
+phases.main = async function(jetpack, argv, phase, current, utils) {
+  const { inspect } = utils;
+
+  const samJson     = await samYamlFile.render(argv);
+  const lambdaJs    = await lambdaJsFile.render(argv);
+
+  await samYamlFile.dest(jetpack).commit(samJson);
+  await lambdaJsFile.dest(jetpack).commit(lambdaJs);
 
   return qm(phase, {
     done: true
   });
 };
+
+exports.main = async function(jetpack, argv, phase, current, utils) {
+  // TODO: read/build a config object from env, setting file in project
+  //       most argv params can be remembered, or fetched, or whatever
+  const handler = utils.getPhaseHandler(phases, phase);
+  return await handler(jetpack, argv, phase, current, utils);
+};
+
